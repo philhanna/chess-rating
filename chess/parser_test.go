93ed188891223119490582567e6324cb4754d036
Phil Hanna <ph1204@gmail.com>
2024-05-23 11:14:12 -0400

    Added body of test

--- chess/parser_test.go
+++ chess/parser_test.go
@@ -2,6 +2,8 @@ package chess
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParse(t *testing.T) {
@@ -15,6 +17,13 @@ func TestParse(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			rating, err := Parse(tt.html)
+			if tt.wantErr {
+				assert.NotNil(t, err)
+			} else {
+				assert.Nil(t, err)
+				assert.Equal(t, tt.want, rating)
+			}
 		})
 	}
 }
