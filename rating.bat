cee8426749afe59bada19f98b81053604a601773
Phil Hanna <ph1204@gmail.com>
2026-05-28 10:33:51 -0400

    Add Windows launcher batch file

---
+++ rating.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+rem Set this to the directory that contains the chess-rating project.
+set "PROJECT_DIR=C:\path\to\chess-rating"
+set "VENV_PYTHON=%PROJECT_DIR%\.venv\Scripts\python.exe"
+
+if not exist "%VENV_PYTHON%" (
+    echo Could not find virtualenv Python at "%VENV_PYTHON%".
+    exit /b 1
+)
+
+pushd "%PROJECT_DIR%" >nul
+"%VENV_PYTHON%" -m rating %*
+set "EXIT_CODE=%ERRORLEVEL%"
+popd >nul
+
+exit /b %EXIT_CODE%
